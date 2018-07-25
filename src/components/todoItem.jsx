2664522720efbb79aa3deba2f65a335f654b270b
Alexander Titovskii <Batonapiton@gmail.com>
2018-07-25 15:30:28 +0300

    TodoItem -> stateless functional component

--- src/components/todoItem.jsx
+++ src/components/todoItem.jsx
@@ -1,46 +1,44 @@
-import React, { Component } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
-class TodoItem extends Component {
-  render() {
-    TodoItem.propTypes = {
-      id: PropTypes.number,
-      value: PropTypes.string,
-      onDelete: PropTypes.func,
-      onCheckboxChange: PropTypes.func,
-      checked: PropTypes.bool
-    };
-    let { id, value, onDelete,onCheckboxChange,checked } = this.props;
 
-    return (
-      <div className="todo-item">
-        <div className="input-group mb-3">
-          <div className="input-group-prepend">
-            <div className="input-group-text">
-              <input
-                type="checkbox"
-                checked={checked}
-                onChange={onCheckboxChange}
-              />
-            </div>
-            <div className="input-group-text">
-              {id}
-            </div>
+const TodoItem = props => {
+  let { id, value, onDelete,onCheckboxChange,checked } = props;
+  return (
+    <div className="todo-item">
+      <div className="input-group mb-3">
+        <div className="input-group-prepend">
+          <div className="input-group-text">
+            <input
+              type="checkbox"
+              checked={checked}
+              onChange={onCheckboxChange}
+            />
           </div>
-          <p className="form-control"> {value}</p>
-          <button
-            className="btn btn-outline-secondary"
-            type="button"
-            onClick={onDelete}
-          >
-            X
-          </button>
+          <div className="input-group-text">
+            {id}
+          </div>
         </div>
+        <p className="form-control"> {value}</p>
+        <button
+          className="btn btn-outline-secondary"
+          type="button"
+          onClick={onDelete}
+        >
+          X
+        </button>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
-
+TodoItem.propTypes = {
+  id: PropTypes.number,
+  value: PropTypes.string,
+  onDelete: PropTypes.func,
+  onCheckboxChange: PropTypes.func,
+  checked: PropTypes.bool
+};
 
 export default TodoItem;
+
